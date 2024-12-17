38bdc83b8f850782c7197fed06a9d1d420afaa78
otastet <o.tastet33@gmail.com>
2024-12-17 15:08:57 -0500

    docker integration

---
+++ deploy-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+set -x
+
+# Build and start the containers
+docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose.yml up -d
+
+# Optional: Clean up old images
+docker image prune -f
