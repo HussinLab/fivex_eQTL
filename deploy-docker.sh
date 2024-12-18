4127c65552b8c3968a791e5388ec708cb4cca3a1
Ubuntu <ubuntu@fivex.arbutus>
2024-12-18 22:08:04 +0000

    add https

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -3,8 +3,8 @@ set -e
 set -x
 
 # Build and start the containers
-docker-compose -f docker-compose.yml build
-docker-compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml build
+docker compose -f docker-compose.yml up -d
 
 # Optional: Clean up old images
 docker image prune -f
